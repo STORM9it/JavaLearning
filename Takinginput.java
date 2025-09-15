74acb5aea177ff456bf163671699fd657bed68d2
Adnan Feeroz <120878923+STORM9it@users.noreply.github.com>
2025-09-15 11:48:22 +0100

    Add files via upload

---
+++ Takinginput.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Takinginput {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+
+
+    }
+
+
+
+}
---
+++ myOneandOnly.java
@@ -0,0 +1,7 @@
+public class myOneandOnly {
+    public static void main(String[] args) {
+    System.out.print("Hi This is Adnan\n and i am doing some experiment\n");
+        System.out.print("*\n**\n***\n****\n*****\n******\n*******\n now this is the First approach to print any values\nand the other approach is\n System.out.println(\"That's the 2nd approach to follow\");\n");
+
+}
+}
